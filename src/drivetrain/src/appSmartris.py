8cfd9a5879f8bd069a8c33f3c459cb4a018c6d43
Diego-Bau <diego-bautist@hotmail.com>
2023-04-25 18:42:07 -0500

    Launch files optimized

--- src/drivetrain/src/appSmartris.py
+++ src/drivetrain/src/appSmartris.py
@@ -12,8 +12,14 @@ class AppSmartris:
 		rospy.Subscriber("/cmd_vel", Twist, self.cmd_Callback)
 		rospy.Subscriber("/Ultra", Float32, self.ultra_fault)
 
+		#60FF value +: antihorario
+		#60FF value -: horario
+
+
 		# variables
-		self.motor_cmd = 0 
+		self.motor_cmd = 0 #velocidad LINE FOLLOWER
+		self.motor_cmdc = 0 #velocidad control
+		self.dist= 0
 		
 		# init node
 		rospy.init_node(nodeName)
@@ -23,10 +29,12 @@ class AppSmartris:
 		self.motor_cmd = msg.linear.y
 		
 	def ultra_fault(self, msg):
-		dist = msg.data
-		
-		if dist < 40: # These are cm
-			self.motor_cmd = 0
+		self.dist = msg.data
+
+#		print(dist)		
+#		if dist < 40: # These are cm
+#			self.motor_cmd = 0
+#			print("Oskyselacome")
 		
 
 	def setVelocity(self):
@@ -39,10 +47,16 @@ class AppSmartris:
 			#get cmd_vel.linear.y value and use service to move motor
 
 			# Check if the ultrasonic is being blocked
-			
-			vel_service_call = self.set_object(node, obj, str(self.motor_cmd * 10), False)
-			print("motor in motion", self.motor_cmd)
-			
+			if self.dist < 40:
+				self.motor_cmdc= 0	
+				print("ALTO")
+				print("Dato enviado: ", self.motor_cmdc)
+				vel_service_call = self.set_object(node, obj, str(self.motor_cmdc * 10), False)
+#			print("motor in motion", self.motor_cmd)
+			else:
+				print("Dato enviado: ", self.motor_cmd)
+				vel_service_call= self.set_object(node, obj, str(self.motor_cmd * 10), False)
+		
 		except rospy.ServiceException as e:
 			# catch any errors
 			print("Service call failed: %s"%e)
@@ -52,7 +66,7 @@ class AppSmartris:
 if __name__ == "__main__":
 	name = "app"
 	node_app = AppSmartris(name)
-
+	velocidad= 0
 	print("Running main ...")
 	while not rospy.is_shutdown():
 	# while node active
